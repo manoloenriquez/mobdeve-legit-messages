d90cb652fd596b8df74e73e81183fdd6e6e0f81d
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-17 17:31:06 +0800

    fragment

--- app/src/main/java/com/mobdeve/s18/legitmessages/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/com/mobdeve/s18/legitmessages/ui/contacts/ContactsFragment.kt
@@ -5,15 +5,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s18.legitmessages.R
 import com.mobdeve.s18.legitmessages.adapter.ContactAdapter
+import com.mobdeve.s18.legitmessages.databinding.FragmentContactsBinding
 import com.mobdeve.s18.legitmessages.model.User
 import com.mobdeve.s18.legitmessages.util.DataHelper
 
@@ -23,17 +26,16 @@ class ContactsFragment : Fragment() {
     private var contactList: ArrayList<User> = ArrayList<User>()
     private lateinit var contactViewModel: ContactViewModel
     private lateinit var contactAdapter: ContactAdapter
-    private lateinit var rv_contacts: RecyclerView
-    private lateinit var binding: ContactsFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_contacts,container, false)
+        val binding: FragmentContactsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_contacts,
+                                                                        container, false)
         contactList = dataHelper.initList()
         contactAdapter = activity?.let { ContactAdapter(contactList, it.applicationContext) }!!
-
-
+        binding.rvContactList.layoutManager = LinearLayoutManager(requireActivity().applicationContext)
+        binding.rvContactList.adapter = contactAdapter
 
         return binding.root
     }
