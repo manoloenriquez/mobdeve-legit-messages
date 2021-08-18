5576495bd6f67fb63fd6305df83fb97ebc6450fe
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-18 15:30:48 +0800

    added register layout

---
+++ app/src/main/java/com/mobdeve/s18/legitmessages/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.mobdeve.s18.legitmessages.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobdeve.s18.legitmessages.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
