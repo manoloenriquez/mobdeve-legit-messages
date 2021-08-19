e07a0e463b701c7bb3fbf113d7f3476087eedb5c
Manolo Enriquez <noloenriquez@gmail.com>
2021-08-20 01:58:06 +0800

    logout feature

--- app/src/main/java/com/mobdeve/s18/legitmessages/model/Database.kt
+++ app/src/main/java/com/mobdeve/s18/legitmessages/model/Database.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s18.legitmessages.model
 
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -15,5 +16,26 @@ class Database {
             "lastName" to user.lastName
         )
         user.uid?.let { db.collection("users").document(it).set(map) }
+    }
+
+    fun getUser(uid: String) {
+        db.collection("users").document(uid).get()
+            .addOnSuccessListener { document ->
+                if (document == null) {
+                    return@addOnSuccessListener
+                }
+
+                Log.i("Database", "User: ${document.data}")
+            }
+    }
+
+    fun updateUser(user: User) {
+        val map = hashMapOf(
+            "username" to user.username,
+            "email" to user.email,
+            "firstName" to user.firstName,
+            "lastName" to user.lastName
+        )
+        user.uid?.let { db.collection("users").document(it).update(map as Map<String, Any>) }
     }
 }
--- app/src/main/java/com/mobdeve/s18/legitmessages/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/s18/legitmessages/ui/login/LoginActivity.kt
@@ -34,12 +34,12 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        if (auth.currentUser != null) {
-//            val intent = Intent(this, MainActivity::class.java)
-//            setResult(Activity.RESULT_OK)
-//            startActivity(intent)
-//            finish()
-//        }
+        if (auth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            setResult(Activity.RESULT_OK)
+            startActivity(intent)
+            finish()
+        }
 
         val username = binding.username
         val password = binding.password
