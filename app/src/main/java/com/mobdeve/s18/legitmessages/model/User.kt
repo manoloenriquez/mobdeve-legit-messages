e02adeb95d11df761d54e05ffefe5706aa43574c
Manolo Enriquez <noloenriquez@gmail.com>
2021-08-20 00:44:51 +0800

    user model

--- app/src/main/java/com/mobdeve/s18/legitmessages/model/User.kt
+++ app/src/main/java/com/mobdeve/s18/legitmessages/model/User.kt
@@ -1,8 +1,30 @@
 package com.mobdeve.s18.legitmessages.model;
 
 class User (val uid: String?, val email: String?, displayName: String?){
-    val displayName: String = ""
     var username: String = ""
+    var firstName: String = ""
+    var lastName: String = ""
 
+    constructor(
+        uid: String?,
+        email: String?,
+        displayName: String?,
+        firstName: String,
+        lastName: String
+    ) : this(uid, email, displayName) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    constructor(
+        uid: String?,
+        email: String?,
+        displayName: String?,
+        firstName: String,
+        lastName: String,
+        username: String,
+    ) : this(uid, email, displayName, firstName, lastName) {
+        this.username = username
+    }
 
 }
